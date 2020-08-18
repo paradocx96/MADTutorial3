082bcdd1e60dc21514389975a2d1e6c1b747332e
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-08-18 22:45:14 +0530

    Final Commit

--- app/src/main/java/com/sliit/intentsproj/MainActivity.java
+++ app/src/main/java/com/sliit/intentsproj/MainActivity.java
@@ -29,21 +29,6 @@ public class MainActivity extends AppCompatActivity {
         etNumber1 = findViewById(R.id.num1);
         etNumber2 = findViewById(R.id.num2);
 
-        //Creating the LayoutInflater instance
-        LayoutInflater li = getLayoutInflater();
-
-        //Getting the View object as defined in the customtoast.xml file
-        View layout = li.inflate(R.layout.customtoast, (ViewGroup)findViewById(R.id.custom_toast_layout));
-
-        //Creating the Toast object
-        Toast toast = new Toast(getApplicationContext());
-        toast.setDuration(Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-
-        //setting the view of custom toast layout
-        toast.setView(layout);
-        toast.show();
-
         OnclickButtonListener();
     }
 
@@ -54,15 +39,29 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View view) {
 
                         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                        intent.putExtra("n1", etNumber1.getText().toString());
+                        intent.putExtra("n2", etNumber2.getText().toString());
 
-                        Context context = getApplicationContext();
+                        /*Context context = getApplicationContext();
                         CharSequence text = "You just clicked the OK button";
                         int duration = Toast.LENGTH_SHORT;
                         toast = Toast.makeText(context, text, duration);
-                        toast.setGravity(Gravity.BOTTOM, 0, 0);
+                        toast.setGravity(Gravity.BOTTOM, 0, 0);*/
 
-                        intent.putExtra("n1", etNumber1.getText().toString());
-                        intent.putExtra("n2", etNumber2.getText().toString());
+                        //Creating the LayoutInflater instance
+                        LayoutInflater li = getLayoutInflater();
+
+                        //Getting the View object as defined in the customtoast.xml file
+                        View layout = li.inflate(R.layout.customtoast, (ViewGroup)findViewById(R.id.custom_toast_layout));
+
+                        //Creating the Toast object
+                        Toast toast = new Toast(getApplicationContext());
+                        toast.setDuration(Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+
+                        //setting the view of custom toast layout
+                        toast.setView(layout);
+                        toast.show();
 
                         startActivity(intent);
                     }
